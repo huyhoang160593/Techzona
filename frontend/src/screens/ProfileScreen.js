87f3e515aec060b9dbe6e9d8eb7478a6694ddac9
iulover99 <iulover99@gmail.com>
2020-08-11 11:23:33 +0700

    fix some warning

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -55,17 +55,17 @@ function ProfileScreen(props) {
                             <label htmlFor="name">
                                 Tên
                             </label>
-                            <input value={name} type="name" name="name" id="name" onChange={(e) => setName(e.target.value)}></input>
+                            <input value={name || ""} type="name" name="name" id="name" onChange={(e) => setName(e.target.value)}></input>
                         </li>
                         <li>
                             <label htmlFor="email">
                                 Email
                             </label>
-                            <input value={email} type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)}></input>
+                            <input value={email || ""} type="email" name="email" id="email" disabled onChange={(e) => setEmail(e.target.value)}></input>
                         </li>
                         <li>
                             <label htmlFor="password">Mật khẩu</label>
-                            <input value={password} type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}></input>
+                            <input value={password || ""} type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}></input>
                         </li>
                         <li>
                             <button type="submit" className="button primary">Cập nhật</button>
